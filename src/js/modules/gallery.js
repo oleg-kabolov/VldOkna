4c01575ac227b474552225f83ea7ef1e1c5588ea
Belsnikel <afterouy@gmail.com>
2026-04-28 16:11:07 +0300

    final

--- src/js/modules/gallery.js
+++ src/js/modules/gallery.js
@@ -2,23 +2,13 @@ const gallery = () => {
   const imgPopup = document.createElement("div");
   const workSection = document.querySelector(".works");
   const bigImage = document.createElement("img");
-  const [arrowLeft, arrowRight] = [
-    document.createElement("div"),
-    document.createElement("div"),
-  ];
-  const [imgLeft, imgRight] = [
-    document.createElement("img"),
-    document.createElement("img"),
-  ];
   const body = document.querySelector("body");
   const imageCollection = document.querySelectorAll("img.preview");
 
   imgPopup.classList.add("popup");
   workSection.appendChild(imgPopup);
-  imgPopup.appendChild(arrowLeft);
-  imgPopup.appendChild(arrowRight);
 
-  bigImage.style.width = "70vw";
+  bigImage.style.width = "60vh";
 
   imgPopup.style.justifyContent = "center";
   imgPopup.style.alignItems = "center";
@@ -32,6 +22,7 @@ const gallery = () => {
     let target = e.target;
 
     if (target && target.classList.contains("preview")) {
+      imageCollection.forEach((img) => {});
       imgPopup.style.display = "flex";
       const path = target.parentNode.getAttribute("href");
       bigImage.setAttribute("src", path);
@@ -41,34 +32,6 @@ const gallery = () => {
       imgPopup.style.display = "none";
       body.style.overflow = "visible";
     }
-  });
-
-  console.log(imageCollection);
-
-  [imgLeft, imgRight].forEach((img) => {
-    img.src = "../assets/img/arrow.png";
-    img.style.width = "10vw";
-  });
-
-  // Добавляем в контейнеры
-  arrowLeft.appendChild(imgLeft);
-  arrowRight.appendChild(imgRight);
-
-  arrowLeft.setAttribute(
-    "style",
-    "position:absolute; top:40vw; left: 1vw; width: 15px; height: 15px; cursor:pointer",
-  );
-  arrowRight.setAttribute(
-    "style",
-    "position:absolute; rotate: 180deg; top:48vw; right: 1vw; width: 15px; height: 15px; cursor:pointer",
-  );
-
-  [arrowLeft, arrowRight].forEach((div) => {
-    div.addEventListener("click", (e) => {
-      let target = e.target;
-      if (target && target.matches(".arror-left")) {
-      }
-    });
   });
 };
 
